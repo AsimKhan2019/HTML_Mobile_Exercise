39af43a3a6e7a8ce554de7418af2d13af5ed459c
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-06-14 20:28:03 +0500

    updated the code according to requirements

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const hBtn = document.getElementById("menubar");
-const nav = document.querySelector("nav");
+const nav = document.getElementById("mobNav");
 function toggleMenu() {
     hBtn.classList.toggle("open");
     nav.classList.toggle("open");
 }
-
 hBtn.addEventListener("click", toggleMenu);
+nav.addEventListener("click", toggleMenu);
+
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
